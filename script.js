6e2fc7292ee0600d60b5de3961a3e5d8813170b9
Apprentice <apprentice@dbc29.local>
2013-10-20 21:58:21 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+   $(document).keyup(function(event){
+      $(".racer_table td").addClass("active")
+  });
+
+   var count = 0
+   var elem = document.getElementById("")
+
+
+});
+
+
+ // setTimeout(function ()
+ //       {
+
+ //       }, 5);
+ //      });
+ //      if(e.key == "Enter")
+ //      {
+ //         //Enter key was pressed, do stuff
+ //      }else if(e.key == "Spacebar")
+ //      {
+ //         //Spacebar was pressed, do stuff
+ //      }
+ //  });
