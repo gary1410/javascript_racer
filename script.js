27895f7315f181b3cb9775e5d213d79d339f33d3
Apprentice <apprentice@dbc13.local>
2013-10-23 22:03:10 -0700

    finished racer

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 $(document).ready(function(){
 
-   $(document).keyup(function(event){
-      $(".racer_table td").addClass("active")
-  });
+  document.addEventListener("keypress", pressKey, false);
 
-   var count = 0
-   var elem = document.getElementById("")
 
+  var counter1 = 0;
+  var counter2 = 0;
+
+  function pressKey(event){
+    if (event.keyCode === 108){
+      var track1 = document.getElementById("player1_strip");
+      active_track = track1.getElementsByTagName('td');
+      counter1 += 1;
+      active_track[counter1].className = "active";
+      removeBlack(counter1 - 1);
+    }
+    else if (event.keyCode === 97 ){
+
+      var track2 = document.getElementById("player2_strip");
+      active_track = track2.getElementsByTagName('td');
+      counter2 += 1;
+      active_track[counter2].className = "active";
+      removeBlack(counter2 - 1);
+    }
+  }
+
+  function removeBlack(column_before){
+    active_track[column_before].classList.remove("active");
+  }
 
 });
 
-
- // setTimeout(function ()
- //       {
-
- //       }, 5);
- //      });
- //      if(e.key == "Enter")
- //      {
- //         //Enter key was pressed, do stuff
- //      }else if(e.key == "Spacebar")
- //      {
- //         //Spacebar was pressed, do stuff
- //      }
- //  });
